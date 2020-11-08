b373e9d7524f5885fc4f3b6c1100b742b43d3c2b
Daniil <pimanov.d@gmail.com>
2020-11-08 14:07:31 +0300

    added tomorrow prediction

--- app.py
+++ app.py
@@ -79,8 +79,7 @@ def main():
 
 	elif request.json['request']['command'] == 'на завтра':
 		tomorrow_date = date.today() + timedelta(days=1)
-		text = request.json['request']['command']
-		# text = "Гороскоп на завтра. \n"+get_prediction(tomorrow_date, user_sign, PREDICTIONS_DF)
+		text = "Гороскоп на сегодня. \n"+get_prediction(today_date, user_sign, PREDICTIONS_DF)
 		buttons = [{"title":"На сегодня"}, {"title":"На другую дату"}]
 
 	elif request.json['request']['command'] == 'на другую дату':
